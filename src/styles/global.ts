f4472376f2576a2bdf52da9d2934ae245b716a84
Ednardo Gomes <ednardo@email.com>
2022-08-17 21:21:15 -0300

    Importando font externa no projeto.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,7 +8,13 @@ export const GlobalStyled = createGlobalStyle`
   }
 
   body {
-    background-color: #333;
-    color: #FFF;
+    background-color: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
   }
 `;
